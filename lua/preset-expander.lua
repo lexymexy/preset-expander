59e11097e2006053250096166443490ec18858ec
hexagoning <hanging.exo@gmail.com>
2025-08-06 15:30:56 +0300

    fix4 ._.

--- lua/preset-expander.lua
+++ lua/preset-expander.lua
@@ -86,9 +86,9 @@ function M.expand()
 
   -- Find the specific instance of the keyword the cursor is on using word boundaries.
   local start_col, end_col
-  local pattern = "\\<" .. vim.pesc(keyword) .. "\\>"
+  local pattern = vim.pesc(keyword)
   local current_pos = math.max(1, cursor_col+1-#keyword)
-  local s, e = string.find(line_content, keyword, current_pos)
+  local s, e = string.find(line_content, pattern, current_pos)
 
   if not s then
     vim.notify("PresetExpand: Could not locate keyword '" .. keyword .. "' under cursor.", vim.log.levels.ERROR)
